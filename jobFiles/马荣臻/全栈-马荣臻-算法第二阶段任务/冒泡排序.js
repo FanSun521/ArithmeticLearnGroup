8639144ab278e82700451663d09c13fdac4eed23
marongzhen <2712268578@qq.com>
2023-07-12 09:29:08 +0800

    全栈-马荣臻-算法第一阶段任务

--- jobFiles/马荣臻/全栈-马荣臻-算法第二阶段任务/冒泡排序.js
+++ jobFiles/马荣臻/全栈-马荣臻-算法第二阶段任务/冒泡排序.js
@@ -3,8 +3,11 @@
  * @return {number[]}
  */
 var sortArray = function (nums) {
+    //重复n次，每次将最大的排到最后
     for (let i = nums.length - 1; i > 0; i--) {
+        //因为每次会排出一个最大的，所以每一次要排列的长度都在减小
         for (let j = 0; j < i; j++) {
+            //按照大小关系排列交换
             if (nums[j] > nums[j + 1]) {
                 nums[j] = (nums[j + 1] + nums[j]) - (nums[j + 1] = nums[j])
             }
--- jobFiles/马荣臻/全栈-马荣臻-算法第二阶段任务/插入排序.js
+++ jobFiles/马荣臻/全栈-马荣臻-算法第二阶段任务/插入排序.js
@@ -4,11 +4,15 @@
  */
 var sortArray = function (nums) {
     let j;
+    //像扑克牌整理手牌一样，保证起始位置开始的数组为升序，逐个将后面的插入到对应位置
     for (let i = 1; i < nums.length; i++) {
+        //key为要插入的数
         let key = nums[i];
+        //判断该插入到哪里，寻找比要插入的数小的数，大的数就往后移动一位
         for (j = i - 1; j >= 0 && nums[j] > key; j--) {
             nums[j + 1] = nums[j];
         }
+        //插入
         nums[j + 1] = key;
     }
     return nums;
